4b40573698880c928366c3f7634c086e686965b7
Leestan360 <stanleymayore2@gmail.com>
2022-05-18 12:15:36 +0300

    Link signup.html to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@ let signUp = document.getElementById('signup')
 let button = document.getElementById('btn');
 
 function signup () {
-    login.style.left = '200px';
-    signUp.style.left = '400px';
-    button.style.left = '200px';
+    login.style.left = '-300px';
+    signUp.style.left = '50px';
+    button.style.left = '90px';
 }
 
 function login () {
-    login.style.left = '200px';
-    signUp.style.left = '400px';
-    button.style.left = '200px';
+    login.style.left = '50px';
+    signUp.style.left = '350px';
+    button.style.left = '0px';
 }
