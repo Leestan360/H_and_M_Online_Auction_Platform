cd3792dced035b9140125a1fd74238ba51371567
Leestan360 <stanleymayore2@gmail.com>
2022-05-18 11:42:08 +0300

    Style the signup button

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let logIn = document.getElementById('login');
+let signUp = document.getElementById('signup')
+let button = document.getElementById('btn');
+
+function signup () {
+    login.style.left = '200px';
+    signUp.style.left = '400px';
+    button.style.left = '200px';
+}
+
+function login () {
+    login.style.left = '200px';
+    signUp.style.left = '400px';
+    button.style.left = '200px';
+}
