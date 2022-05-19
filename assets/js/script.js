5d3ce359cc99b8e054e300502d3be5a9853906f0
Leestan360 <stanleymayore2@gmail.com>
2022-05-19 09:27:04 +0300

    update project

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const email = document.getElementById('first');
+const password = document.getElementById('second');
+
+submitBtn.addEventListener('click', () => {
+    const password = password;
+
+    if (!password) {
+        alert('passwords do not match');
+    } else {
+        alert('Welcome to H & M Online Auction')};
+})
